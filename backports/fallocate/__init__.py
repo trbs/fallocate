150c0a2a45f5ab9b36c501536e4f9e6e32582dc8
trbs <trbs@trbs.net>
2013-09-01 00:03:41 +0200

    add wrapper to allow automatically unboxing file object to fileno

--- backports/fallocate/__init__.py
+++ backports/fallocate/__init__.py
@@ -2,12 +2,30 @@
 __version__ = "1.0"
 
 try:
-    from ._fallocate import fallocate
+    from ._fallocate import fallocate as real_fallocate
+    def fallocate(fd, mode, offset, len):
+        if isinstance(fd, file):
+            fd = fd.fileno()
+        return real_fallocate(fd, mode, offset, len)
+    fallocate.__doc__ = real_fallocate.__doc__
 except ImportError:
     pass
+
 try:
-    from ._fallocate import posix_fallocate, posix_fadvise
+    from ._fallocate import posix_fallocate as real_posix_fallocate
+    from ._fallocate import posix_fadvise as real_posix_fadvise
     from ._fallocate import POSIX_FADV_NORMAL, POSIX_FADV_SEQUENTIAL, POSIX_FADV_RANDOM, POSIX_FADV_NOREUSE, POSIX_FADV_WILLNEED, POSIX_FADV_DONTNEED
+    def posix_fallocate(fd, offset, len):
+        if isinstance(fd, file):
+            fd = fd.fileno()
+        return real_possix_fallocate(fd, offset, len)
+    posix_fallocate.__doc__ = real_posix_fallocate.__doc__
+
+    def posix_fadvise(fd, offset, len, advice):
+        if isinstance(fd, file):
+            fd = fd.fileno()
+        return real_posix_fadvise(fd, offset, len, advice)
+    posix_fadvise.__doc__ = real_posix_fadvise.__doc__
 except ImportError:
     pass
 
