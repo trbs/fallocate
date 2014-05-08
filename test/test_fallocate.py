42b978eed05f5409d577fd614034a954ecee3961
trbs <trbs@trbs.net>
2014-05-08 14:46:34 +0200

    add tests

---
+++ test/test_fallocate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import platform
+import tempfile
+from fallocate import fallocate
+
+def simple_fallocate_1kb_test():
+    with tempfile.NamedTemporaryFile() as ntf:
+        assert os.path.getsize(ntf.name) == 0
+        fallocate(ntf, 0, 1024)
+        assert os.path.getsize(ntf.name) == 1024
+
+def simple_fallocate_1mb_test():
+    with tempfile.NamedTemporaryFile() as ntf:
+        assert os.path.getsize(ntf.name) == 0
+        fallocate(ntf, 0, 1024*1024)
+        assert os.path.getsize(ntf.name) == 1024*1024
+
+if platform.system() == "Linux":
+    from fallocate import FALLOC_FL_KEEP_SIZE, FALLOC_FL_PUNCH_HOLE
+    def fallocate_punch_hole_test():
+        with tempfile.NamedTemporaryFile() as ntf:
+            assert os.path.getsize(ntf.name) == 0
+            ntf.write("Hello World")
+            ntf.flush()
+            ntf.seek(0)
+            assert ntf.read() == "Hello World"
+            fallocate(ntf.fileno(), 6, 4, mode=FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE)
+            ntf.seek(0)
+            assert ntf.read() == "Hello \x00\x00\x00\x00d"
